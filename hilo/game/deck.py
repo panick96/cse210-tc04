ef55cfafa73a78344927c05ed8713ec37732c4d7
Nick Velasquez <nvelasquez596@gmail.com>
2021-01-29 15:57:53 -0700

    returns a card from draw_card function

--- hilo/game/deck.py
+++ hilo/game/deck.py
@@ -6,12 +6,15 @@ class Deck:
     
 
     def __init__(self) -> None:
-        self.deck = []
-        
+        self.deck = []        
         self.card_choice = 0
         
 
     def create_deck (self):
+        """
+        args : instance of deck
+        creates a deck and assigns it to self.deck
+        """
         counter = 0
 
         for card in range(10):            
@@ -19,15 +22,20 @@ class Deck:
             self.deck.append(counter)
     
     def draw_card(self):
+        """
+        args : instance of deck
+        assigns card_choice to an integer
+        """
         choice = random.choice(self.deck)
         index = choice - 1
         card = self.deck[index]
         self.card_choice = card
         del self.deck[index]
+        return self.card_choice
 
         
             
-
+"""
 test = Deck()
 
 test.create_deck()
@@ -36,3 +44,4 @@ print(test.deck)
 test.draw_card()
 print(test.card_choice)       
 print(test.deck)
+"""
